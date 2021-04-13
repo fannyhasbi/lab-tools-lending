6affbbf8365a8f6afd506d280b85b26951c7f180
Hasbi <fnyhsbi@gmail.com>
2021-04-13 18:36:46 +0700

    singleton DB

--- config/database.go
+++ config/database.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"sync"
 
 	"os"
 
@@ -12,31 +13,38 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+var dbInit sync.Once
+var dbInstance *sql.DB
+
 func InitPostgresDB() *sql.DB {
-	DBHost := os.Getenv("DB_HOST")
-	DBDriver := os.Getenv("DB_DRIVER")
-	DBUser := os.Getenv("DB_USER")
-	DBPass := os.Getenv("DB_PASSWORD")
-	DBPort := os.Getenv("DB_PORT")
-	DBName := os.Getenv("DB_NAME")
+	dbInit.Do(func() {
+		DBHost := os.Getenv("DB_HOST")
+		DBDriver := os.Getenv("DB_DRIVER")
+		DBUser := os.Getenv("DB_USER")
+		DBPass := os.Getenv("DB_PASSWORD")
+		DBPort := os.Getenv("DB_PORT")
+		DBName := os.Getenv("DB_NAME")
 
-	connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable", DBDriver, DBUser, DBPass, DBHost, DBPort, DBName)
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
+		connStr := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable", DBDriver, DBUser, DBPass, DBHost, DBPort, DBName)
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	m, err := migrate.New(
-		"file://database/migration",
-		connStr,
-	)
-	if err != nil {
-		log.Fatal(err)
-	}
+		m, err := migrate.New(
+			"file://database/migration",
+			connStr,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
-	}
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
 
-	return db
+		dbInstance = db
+	})
+
+	return dbInstance
 }
