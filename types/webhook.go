d29c53e50caa47b82f1ad5f0da7ee781d0f3dc3d
Hasbi <fnyhsbi@gmail.com>
2021-09-05 02:57:57 +0700

    make a new own struct for webhook request

--- types/webhook.go
+++ types/webhook.go
@@ -3,33 +3,35 @@ package types
 type (
 	RequestType string
 
+	teleMessageFrom struct {
+		ID int64 `json:"id"`
+	}
+
+	teleMessageChat struct {
+		ID   int64  `json:"id"`
+		Type string `json:"type"`
+	}
+
+	teleMessage struct {
+		MessageID int64           `json:"message_id"`
+		From      teleMessageFrom `json:"from"`
+		Text      string          `json:"text"`
+		Chat      teleMessageChat `json:"chat"`
+	}
+
 	WebhookRequest struct {
-		Message struct {
-			From struct {
-				ID int64 `json:"id"`
-			}
-			Text string `json:"text"`
-			Chat struct {
-				ID   int64  `json:"id"`
-				Type string `json:"type"`
-			} `json:"chat"`
-		} `json:"message"`
+		Message teleMessage `json:"message"`
+	}
+
+	teleCallbackQuery struct {
+		ID      string          `json:"id"`
+		From    teleMessageFrom `json:"from"`
+		Message teleMessage     `json:"message"`
+		Data    string          `json:"data"`
 	}
 
 	InlineCallbackQuery struct {
-		CallbackQuery struct {
-			ID   string `json:"id"`
-			From struct {
-				ID int64 `json:"id"`
-			} `json:"from"`
-			Message struct {
-				Chat struct {
-					ID   int64  `json:"id"`
-					Type string `json:"type"`
-				} `json:"chat"`
-			} `json:"message"`
-			Data string `json:"data"`
-		} `json:"callback_query"`
+		CallbackQuery teleCallbackQuery `json:"callback_query"`
 	}
 )
 
